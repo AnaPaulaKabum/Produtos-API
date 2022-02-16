c05f55255bbee20565a8c450dc7088e126db178b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-02-16 10:19:47 -0300

    Adiciona swagger e realiza a documentacao

--- src/produto.model.ts
+++ src/produto.model.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Table,Model,Column, DataType } from "sequelize-typescript";
 
 @Table
 export class Produto extends Model{
 
     @Column({type:DataType.STRING(60), allowNull:false})
+    @ApiProperty({ example: "LIV001"})
     codigo:string;
 
     @Column({type: DataType.STRING, allowNull:false})
+    @ApiProperty({ example: "Livro NestJS"})
     nome:string;
 
     @Column({ type: DataType.DECIMAL, allowNull:false})
+    @ApiProperty({ example: 19.99})
     preco:number;
 }
