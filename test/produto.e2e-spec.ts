1f10a4c7648b9da53bc49383f2265b4ed6b2dd5b
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-03-10 10:22:15 -0300

    Refatora Teste de Integraçao /produtos (GET)

--- test/produto.e2e-spec.ts
+++ test/produto.e2e-spec.ts
@@ -2,27 +2,37 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { ProdutoRepository } from '../src/database/remote/repository/produtoRepository';
 
 describe('ProdutoController (e2e)', () => {
   let app: INestApplication;
 
+  let produtoRepository = { obterTodos: () => ['teste'] };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+    .overrideProvider(ProdutoRepository)
+    .useValue(produtoRepository)
+    .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await app.close();
   });
 
   it('/produtos (GET) :', () => {
     return request(app.getHttpServer())
       .get('/produtos')
-      .expect(200)
-      //.expect('Hello World!');
+      .then((result) => {
+        expect(result.statusCode).toEqual(200);
+        expect(result.body).toEqual(['teste']);
+      });
+
   });
+
 });
