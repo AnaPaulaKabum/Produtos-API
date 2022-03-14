17e1933663db59b09707fa80d4f72e110d31bd4c
AnaPaulaKabum <ana.desouza@kabum.com.br>
2022-03-14 17:22:37 -0300

    Refacotr produto providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProdutosController } from './application/controller/produtos.controller';
 import { configConexao } from './database/configConexao';
-import { ProdutosServices } from './services/produtos.service';
 import { ConfigModule } from '@nestjs/config';
-import { ProdutoRepository } from './database/remote/repository/produtoRepository';
-import { photoProviders } from './database/remote/providers/produto.provider';
+import { produtoProviders } from './database/remote/providers/produto.provider';
 
 @Module({
   imports : [ConfigModule.forRoot()],
   controllers: [ProdutosController],
-  providers:   [...configConexao,...photoProviders,ProdutoRepository,ProdutosServices],
+  providers:   [...configConexao,...produtoProviders],
 })
 export class AppModule {}
--- src/database/remote/providers/produto.provider.ts
+++ src/database/remote/providers/produto.provider.ts
@@ -1,10 +1,14 @@
 import { Connection } from 'typeorm';
 import { ProdutoEntity } from '../entity/produto.entity';
+import { ProdutosServices } from '../../../services/produtos.service';
+import { ProdutoRepository } from '../../../database/remote/repository/produtoRepository';
 
-export const photoProviders = [
+export const produtoProviders = [
   {
     provide: 'PRODUTO_REPOSITORY',
     useFactory: (connection: Connection) => connection.getRepository(ProdutoEntity),
     inject: ['DATABASE_CONNECTION'],
   },
+  ProdutoRepository,
+  ProdutosServices
 ];
